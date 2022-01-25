95ac902a602b72f0c47ebe19ca94f4abbf060b71
adev <czasacz@gmail.com>
2022-01-25 11:49:08 +0100

    Finished styling sidebar section.

---
+++ linkedin-clone-yt/src/Feed.js
@@ -0,0 +1,7 @@
+import "./Feed.css";
+
+function Feed() {
+  return <div className="feed"></div>;
+}
+
+export default Feed;
--- linkedin-clone-yt/src/Sidebar.js
+++ linkedin-clone-yt/src/Sidebar.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import './Sidebar.css';
+import React from "react";
+import "./Sidebar.css";
+import Avatar from "@mui/material/Avatar";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
-    <div className='sidebar'>
-        <h1>I am a sidebar</h1>
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1504060765228-f97d1772ff9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Arkadiusz Biesiada</h2>
+        <h4>arkadiuszbiesiada@gmail.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,448</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('programming')}
+        {recentItem('softwareengineering')}
+        {recentItem('design')}
+        {recentItem('developer')}
+      </div>
     </div>
   );
 }
