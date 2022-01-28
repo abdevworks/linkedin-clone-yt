07d89fb9bf0240f0a701245e864de5768ed0c1de
adev <czasacz@gmail.com>
2022-01-28 09:42:42 +0100

    Firebase config - database

--- linkedin-clone-yt/src/Feed.js
+++ linkedin-clone-yt/src/Feed.js
@@ -1,7 +1,92 @@
 import "./Feed.css";
+import CreateIcon from "@mui/icons-material/Create";
+import InputOption from "./InputOption";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+import Post from "./Post";
+import { useEffect, useState } from "react";
+import { db } from "./firebase";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
 function Feed() {
-  return <div className="feed"></div>;
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      console.log(data);
+      setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, []);
+
+  const sendPost = async (e) => {
+    e.preventDefault();
+
+    await addDoc(postsCollectionRef, {
+      name: "Arkadiusz Biesiada",
+      description: "this is a test",
+      message: input,
+      photoUrl: "",
+      // timestamp: FieldValue.serverTimestamp(),
+    });
+  };
+
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+
+      {/* {Posts} */}
+      {posts.map(({id, name, description, message, photoUrl}) => (
+        <Post
+          key={id}
+          id={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
+      {/* {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))} */}
+    </div>
+  );
 }
 
 export default Feed;
